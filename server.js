6f937d4ca99d9f695ced76ce645fb6a115bd4c03
firstlightcreations26-rgb <first.light.creations26@gmail.com>
2026-07-04 14:37:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+let dir = __dirname;
+if (fs.existsSync(path.join(__dirname, 'ilikeitdeep-EASY'))) {
+  dir = path.join(__dirname, 'ilikeitdeep-EASY');
+}
+app.use(express.static(dir));
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('ilikeitdeep is live on port ' + port));
+app.listen(port, () => console.log('ilikeitdeep live'));
