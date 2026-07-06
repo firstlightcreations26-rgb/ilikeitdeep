c48b146a23e2a87282d931f7cc1ef4c6fc829510
firstlightcreations26-rgb <first.light.creations26@gmail.com>
2026-07-06 17:09:10 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('ilikeitdeep is live on port ' + port));
+app.use(express.static(__dirname));
+app.listen(process.env.PORT || 3000, () => console.log('ilikeitdeep is live'));
